464dd792db9f822fbee411257944a3b82b706120
marcoswlrich <marcosapw.dev@gmail.com>
2022-03-29 00:25:10 -0300

    Implementacao de transferencias

--- script.js
+++ script.js
@@ -160,6 +160,29 @@ btnLogin.addEventListener('click', function (e) {
     updateUI(currentAccount)
   }
 })
+
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault()
+  const amount = Number(inputTransferTo.value)
+  const receiverAcc = accounts.find(
+    (acc) => acc.username === inputTransferTo.value
+  )
+  inputTransferAmount.value = inputTransferTo.value = ''
+
+  if (
+    amount > 0 &&
+    receiverAcc &&
+    currentAccount.balance >= amount &&
+    receiverAcc?.username !== currentAccount.username
+  ) {
+    // Doing the transfer
+    currentAccount.movements.push(-amount)
+    receiverAcc.movements.push(amount)
+
+    // Update UI
+    updateUI(currentAccount)
+  }
+})
 // LECTURES
 
 const currencies = new Map([
